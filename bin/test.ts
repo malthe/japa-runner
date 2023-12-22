7c67cda9d1e2034836be5078a3fb0c6559a8bcbe
Malthe Borch <mborch@gmail.com>
2023-12-22 09:54:23 +0100

    Add Deno test

--- bin/test.ts
+++ bin/test.ts
@@ -1,8 +1,9 @@
 import { configure, processCLIArgs, run } from '@japa/runner';
 import { assert } from '@japa/assert'
 import { expect } from '@japa/expect';
+import { argv } from 'node:process';
 
-processCLIArgs(process.argv.splice(2));
+processCLIArgs(argv.splice(2));
 
 configure({
   files: ['tests/**/*.test.ts'],
